14d44a49f5aa8ec01a733b52f03ed1bcecade257
Arto Bendiken <arto@near.org>
2021-04-18 21:34:23 +0300

    Implement `Account.parse()`.

--- lib/account.d.ts
+++ lib/account.d.ts
@@ -1,8 +1,10 @@
+import { Result } from '@hqoss/monads';
 export declare type AccountID = string;
 export declare type Address = string;
 export declare class Account {
     readonly id: string;
     constructor(id: string);
+    static parse(id: string): Result<Account, string>;
     toString(): string;
     toAddress(): Address;
 }
--- src/account.ts
+++ src/account.ts
@@ -1,6 +1,8 @@
 /* This is free and unencumbered software released into the public domain. */
 
+import { getAddress as parseAddress } from '@ethersproject/address';
 import { keccak256 } from '@ethersproject/keccak256';
+import { Result, Ok } from '@hqoss/monads';
 
 export type AccountID = string;
 export type Address = string;
@@ -8,11 +10,16 @@ export type Address = string;
 export class Account {
   constructor(public readonly id: string) {} // TODO: validate the ID
 
+  static parse(id: string): Result<Account, string> {
+    return Ok(new Account(id));
+  }
+
   toString(): string {
     return this.id;
   }
 
   toAddress(): Address {
-    return `0x${keccak256(Buffer.from(this.id)).slice(26, 66)}`;
+    const address = keccak256(Buffer.from(this.id)).slice(26, 66);
+    return `0x${parseAddress(address)}`;
   }
 }
