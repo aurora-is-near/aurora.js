46df44a17a77a7009acad8c1f920da0c92068378
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2021-09-10 14:47:49 +0200

    Fix the encoding of the input data field. (#19)

--- lib/engine.d.ts
+++ lib/engine.d.ts
@@ -5,7 +5,6 @@ import { KeyStore } from './key_store.js';
 import { Quantity, Result, U256 } from './prelude.js';
 import { SubmitResult, OutOfGas } from './schema.js';
 import { TransactionID } from './transaction.js';
-import { Buffer } from 'buffer';
 import NEAR from 'near-api-js';
 import { ResErr } from '@hqoss/monads/dist/lib/result/result';
 export { getAddress as parseAddress } from '@ethersproject/address';
--- src/transaction.ts
+++ src/transaction.ts
@@ -4,7 +4,13 @@ import { AccountID, Address } from './account.js';
 import NEAR, { NEARTransaction } from './near.js';
 import { None, Option, Some, U64, U256 } from './prelude.js';
 import { SubmitResult } from './schema.js';
-import { base58ToBytes, base58ToHex, bytesToHex, intToHex } from './utils.js';
+import {
+  base58ToBytes,
+  base58ToHex,
+  bytesToHex,
+  hexToBytes,
+  intToHex,
+} from './utils.js';
 
 import { parse as parseRawTransaction } from '@ethersproject/transactions';
 
@@ -107,7 +113,7 @@ export class Transaction {
           BigInt(transaction.gasLimit.toString()), // FIXME: #16, #17
           Address.parse(transaction.to).ok(),
           BigInt(transaction.value.toString()),
-          Buffer.from(transaction.data, 'hex'),
+          hexToBytes(transaction.data),
           BigInt(transaction.v),
           BigInt(transaction.r),
           BigInt(transaction.s),
